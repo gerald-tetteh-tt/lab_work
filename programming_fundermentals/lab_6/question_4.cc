bcef92c1f14f83976fade62156daf6ae1795ac1a
Gerald Addo-Tetteh <gerald.addo-tetteh@turntabl.io>
2023-03-27 11:33:57 +0000

    complete question 4 lab 6 of programming fundermentals

---
+++ programming_fundermentals/lab_6/question_4.cc
@@ -0,0 +1,29 @@
+// programming fundamentals: pointers and arrays
+// Question 4
+
+#include <iostream>
+#include <cstring>
+
+int main()
+{
+    char *title1 = "Now is the time";
+    const char *title2 = "For all good men";
+    char *const title3 = "To come to the aid";
+    const char *const title4 = "Of their party.";
+
+    // strcpy(title1, title2);
+    // strcpy(title1, title3);
+    // strcpy(title1, title4);
+
+    // strcpy(title2, title1);
+    // strcpy(title2, title3);
+    // strcpy(title2, title4);
+
+    // strcpy(title3, title1);
+    // strcpy(title3, title2);
+    // strcpy(title3, title4);
+
+    // strcpy(title4, title1);
+    // strcpy(title4, title2);
+    // strcpy(title4, title3);
+}
